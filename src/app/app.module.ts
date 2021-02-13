77913b43c5725133182fa627e48d1ffe7edbfdd9
Dead4W <dead4w@gmail.com>
2021-02-13 11:23:42 +0000

    Stage 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {ProjectBlockComponent} from './project-block/project-block.component';
+import {TodoCreatorComponent} from './todo-creator/todo-creator.component';
+import { HttpClientModule } from '@angular/common/http';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectBlockComponent,
+    TodoCreatorComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatIconModule,
+    HttpClientModule,
+    MatCheckboxModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [TodoCreatorComponent]
 })
 export class AppModule { }
